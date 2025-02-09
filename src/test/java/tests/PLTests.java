192b606816ed352a4ef97cc6f223427d8536b270
keerthipoornaguntaka <keerthipoornaguntaka@gmail.com>
2025-02-09 22:55:39 +0530

    PL cart verification

---
+++ src/test/java/tests/PLTests.java
@@ -0,0 +1,40 @@
+package tests;
+
+import PoundLandFrameWork.PLBase;
+import PoundLandFrameWork.CartPage;
+import PoundLandFrameWork.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class PLTests extends PLBase {
+    HomePage homePage;
+    CartPage cartPage;
+    @BeforeClass
+    public void initializePages(){
+        homePage=new HomePage(driver);
+        cartPage=new CartPage(driver);
+    }
+    @Test
+    public void verifyingProductSearchAndAddToCart() throws InterruptedException {
+
+        homePage.clickOnProductSearchBar();
+        homePage.clickOnCaptchaButton();
+        Thread.sleep(5000);
+        homePage.clickOnSearchIcon();
+        String searchProductText=homePage.getProductText();
+        String searchProductPrice=homePage.getProductPrice();
+        System.out.println(searchProductText);
+        System.out.println(searchProductPrice);
+        homePage.clickOnAddButton();
+        Thread.sleep(5000);
+        cartPage.clickOnCartButton();
+        String addProductText=cartPage.getCartProductText();
+        String addProductPrice=cartPage.getCartProductPrice();
+        System.out.println(addProductText);
+        System.out.println(addProductPrice);
+
+        Assert.assertEquals(searchProductText,addProductText);
+        Assert.assertEquals(searchProductPrice,addProductPrice);
+    }
+}
